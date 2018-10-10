f84bdddadedfef0e081a0c63b835c0f64c7e6fca
ShotaKitazawa <skitazawa1121@gmail.com>
2018-10-10 17:41:08 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# About
+
+text -> md for Paper
+
+# Setup
+
+## For Linux
+
+```
+pip install -r requirements.txt
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+tar xf phantomjs-2.1.1-linux-x86_64.tar.bz2 && rm -f phantomjs-2.1.1-linux-x86_64.tar.bz2
+mv phantomjs-2.1.1-linux-x86_64/bin/phantomjs . && rm -rf phantomjs-2.1.1-linux-x86_64
+```
+
+## For Mac
+
+```
+pip install -r requirements.txt
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-macosx.zip
+tar xf phantomjs-2.1.1-linux-x86_64.tar.bz2 && rm -f phantomjs-2.1.1-linux-x86_64.tar.bz2
+mv phantomjs-2.1.1-linux-x86_64/bin/phantomjs . && rm -rf phantomjs-2.1.1-linux-x86_64
+```
+
+# Execute
+
+```
+python3 translate.py hoge.txt | tee hoge.md
+```
+
---
+++ translate.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import time
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import urllib.parse
+
+
+driver = webdriver.PhantomJS(executable_path='./phantomjs')
+
+
+def translate(en):
+
+    url_text = "https://translate.google.co.jp/#en/ja/{0}".format(en)
+    url = urllib.parse.quote_plus(url_text, "/:?=&#")
+
+    driver.get(url)
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    time.sleep(1.5)
+
+    return soup.find(id='result_box').text.rsplit("...")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("invalid arguments")
+        sys.exit(1)
+
+    filepath = sys.argv[1]
+
+    with open(filepath, 'r') as rfile:
+        for line in rfile:
+            line = line.rstrip('\r\n')
+
+            if len(line) == 0:
+                print()
+            elif line[0].startswith("#"):
+                print(line)
+            elif line[0].startswith("*"):
+                print("PASTE")
+                print(line)
+            else:
+                sentences = line.split(". ")
+                for sentence in sentences:
+                    sentence = sentence + "."
+                    print("```\n" + sentence + "\n```")
+                    print()
+                    print(translate(sentence))
+                    print()
